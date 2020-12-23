b7c7e4667e7b19348954e7da0acf24056723098e
Paul Barroso <paulbg1993@gmail.com>
2020-12-23 11:18:07 +0100

    Fixed errors
    --0.0.4
    
    -Submission form works and paint elements in HTML
    -Added functionality to edit
    -*Edit button needs to be fixed to save the actual edit

--- user/resources/js/mascots.js
+++ user/resources/js/mascots.js
@@ -1,16 +1,32 @@
+/*
+ ** @description Variables form HTML
+ */
+const form = document.getElementById('form-modal');
+const mascotName = document.getElementById('name-animal-form');
+const mascotOwner = document.getElementById('owner-animal-form');
+const mascotType = document.getElementById('type-animal-form');
+const indexModal = document.getElementById('index-modal');
+const submitBtn = document.getElementById('submit-btn');
+
+/*
+ ** @description Immutable object that will not change
+ */
 let mascots = [
   {
     type: '',
     name: '',
     owner: '',
+    index: '',
   },
 ];
 
-const form = document.getElementById('form-modal');
-
-function listMascots() {
+/*
+ ** @description function to render the mascots on the DOM
+ ** @returns and HTML filled with the data from the form
+ */
+function paintMascots() {
   const tableList = document.getElementById('mascots-table');
-  let mascotsHTML = mascots
+  let printMascots = mascots
     .map(
       (mascot, index) =>
         `<tr>
@@ -29,20 +45,73 @@ function listMascots() {
               </select>
             </td>
             <td>
-              <button type="button" class="btn btn-info">
+              <button type="button" class="btn btn-info edit" data-index=${index} data-toggle="modal"
+                data-target="#modal">
                 <i class="far fa-edit"></i>
               </button>
-              <button type="button" class="btn btn-danger">
+              <button type="button" class="btn btn-danger delete" data-index=${index}>
                 <i class="far fa-trash-alt"></i>
               </button>
             </td>
           </tr>`
     )
     .join('');
-  tableList.innerHTML = mascotsHTML;
+  tableList.innerHTML = printMascots;
+
+  Array.from(document.getElementsByClassName('btn btn-info edit')).forEach(
+    (btnEdit, index) => (btnEdit.onclick = edit(index))
+  );
+
+  Array.from(document.getElementsByClassName('btn btn-danger delete')).forEach(
+    (btnDelete, index) => (btnDelete.onclick = deleteData(index))
+  );
+
+  /*
+   ** @description function to edit the mascots
+   ** @returns the inside handler function so it will be execute when we click the edit button
+   ** @params {i} the index assigned to the mascots on the HTML
+   */
+
+  function edit(i) {
+    const handler = () => {
+      submitBtn.innerHTML = 'Save changes';
+      $('#modal').modal('toggle');
+      mascotName.value = mascots[i].name;
+      mascotOwner.value = mascots[i].owner;
+      mascotType.value = mascots[i].type;
+      indexModal.value = mascots[i];
+    };
+    return handler;
+  }
+
+  function deleteData(element) {
+    const handler = () => console.log(element);
+    return handler;
+  }
 }
 
-function createMascot(e) {
+/*
+ ** @description function to submit the form with data
+ ** @returns a new array and calls the {function} paintMascots
+ ** @params {e} to prevent the change of URL and auto-submit
+ */
+function submitForm(e) {
   e.preventDefault();
-  console.log(e);
+  let data = {
+    type: mascotType.value,
+    name: mascotName.value,
+    owner: mascotOwner.value,
+  };
+  const actionToPerform = submitBtn.innerHTML;
+  switch (actionToPerform) {
+    case 'Save changes':
+      console.log(indexModal.value); //need to fix this
+      break;
+    default:
+      mascots.push(data);
+      break;
+  }
+  paintMascots();
 }
+
+form.onsubmit = submitForm;
